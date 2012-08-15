39216d10ba14145fb9aa3797d1bb2f0bc947b941
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-08-15 16:43:11 -0600

    Add tmp file content helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,12 @@ gem 'minitest'
 require 'yacl'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'tmpdir'
+
+module SpecHelpers
+  def self.tmpfile_with_contents( full_path, contents )
+    File.open ( full_path, "w+" ) do |f|
+      f.write( contents )
+    end
+  end
+end

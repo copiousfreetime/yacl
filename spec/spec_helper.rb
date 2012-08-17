a46c7d80a627a7043678dee6fc6486147401b9e4
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-08-17 16:13:57 -0600

    Add spec helpers for accessing directories in the project directory structure

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,16 @@ require 'tmpdir'
 
 module SpecHelpers
   def self.tmpfile_with_contents( full_path, contents )
-    File.open ( full_path, "w+" ) do |f|
+    File.open( full_path, "w+" ) do |f|
       f.write( contents )
     end
+  end
+
+  def self.spec_dir( *other )
+    ::File.join( proj_root, 'spec', other )
+  end
+
+  def self.proj_root
+    ::File.expand_path( "../../", __FILE__)
   end
 end

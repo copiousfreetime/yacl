83364d6cd39c7f7d9b8dcbfabebe2d23daa61712
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-08-27 15:36:41 -0600

    extract common test data to an instance variable

--- spec/properties_spec.rb
+++ spec/properties_spec.rb
@@ -2,21 +2,23 @@ require 'spec_helper'
 require 'yacl/properties'
 
 describe 'Yacl::Properties' do
+  before do
+    @properties = Yacl::Properties.new(  'my.a' => 'foo', 'my.b' => 'bar', 'other.c' => 'baz' )
+  end
   it "can be initialized with a hash" do
     p = Yacl::Properties.new(  'a' => 'foo', 'b' => 'bar'  )
     p['a'].must_equal 'foo'
   end
 
   it "can be scoped by a prefix" do
-    p = Yacl::Properties.new(  'my.a' => 'foo', 'my.b' => 'bar', 'other.c' => 'baz' )
-    s = p.scoped_by( 'my' )
+    s = @properties.scoped_by( 'my' )
     s['a'].must_equal 'foo'
     s['b'].must_equal 'bar'
     s.has_key?( 'c' ).must_equal false
   end
 
   it "can say what its scopes are" do
-    p = Yacl::Properties.new(  'my.a' => 'foo', 'my.b' => 'bar', 'other.c' => 'baz' )
-    p.scopes.must_equal %w[ my other ]
+    @properties.scopes.must_equal %w[ my other ]
+  end
   end
 end

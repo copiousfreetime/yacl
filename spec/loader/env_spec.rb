9e4d73e5714cde84700c1085cf78cfd230fc7247
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-08-27 17:01:25 -0600

    convert to using dotted method call notation.

--- spec/loader/env_spec.rb
+++ spec/loader/env_spec.rb
@@ -5,25 +5,25 @@ describe Yacl::Loader::Env do
     env = { 'MY_APP_A' => 'foo', 'MY_APP_B' => 'bar' }
     e = Yacl::Loader::Env.new( :env => env )
     p = e.properties
-    p['my.app.a'].must_equal 'foo'
-    p['my.app.b'].must_equal 'bar'
+    p.my.app.a.must_equal 'foo'
+    p.my.app.b.must_equal 'bar'
   end
 
   it "uses all keys when there is no prefix" do
     env = { 'MY_APP_A' => 'foo', 'MY_APP_B' => 'bar', "SOMETHING_ELSE" => 'baz' }
     e = Yacl::Loader::Env.new( :env => env )
     p = e.properties
-    p['my.app.a'].must_equal 'foo'
-    p['my.app.b'].must_equal 'bar'
-    p['something.else'].must_equal 'baz'
+    p.my.app.a.must_equal 'foo'
+    p.my.app.b.must_equal 'bar'
+    p.something.else.must_equal 'baz'
   end
 
   it "uses only those keys with the given prefix and strips that prefix" do
     env = { 'MY_APP_A' => 'foo', 'MY_APP_B' => 'bar', "SOMETHING_ELSE" => 'baz' }
     e = Yacl::Loader::Env.new( :env => env, :prefix => "my.app" )
     p = e.properties
-    p['a'].must_equal 'foo'
-    p['b'].must_equal 'bar'
+    p.a.must_equal 'foo'
+    p.b.must_equal 'bar'
     p.has_key?('c').must_equal false
   end
 end

c304b0ab95e14e7a1f2c223e5d8cf840334f8517
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-08-28 10:52:39 -0600

    Add Loader specs.

---
+++ spec/loader_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+require 'yacl/loader'
+
+describe 'Yacl::Loader' do
+  before do
+    @loader = Yacl::Loader.new( :foo => 'bar', :baz => 'wibble' )
+  end
+  it "is initialized with options" do
+    @loader.options[:foo].must_equal 'bar'
+  end
+
+  it "returns a Properties instance" do
+    p = @loader.properties
+    p.sizes.must_equal 0
+  end
+end

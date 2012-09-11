8bab19c4ccdbf9a76a9fbcfd14bac0e2a5bbef3f
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-09-11 16:22:36 -0600

    TomDocify and remove banner methods.

--- lib/yacl/define/cli/options.rb
+++ lib/yacl/define/cli/options.rb
@@ -1,44 +1,90 @@
 module Yacl::Define::Cli
 
+  # Internal: Encapsulation of all the elements pertaining to a single
+  # commandline option.
   Option = Struct.new( :property_name, :long, :short, :description, :cast)
 
+  # Public: Inherit from Cli::Options to define your the mapping between your
+  # application properties and commandline switches.
+  #
+  # The class you create from here is to be used in conjuction with a class you
+  # create that is a child class of Cli::Parser.
+  #
+  # Example:
+  #
+  #   class MyOptions < ::Yacl::Define::Cli::Options
+  #     opt 'log.level', :long => 'log-level', :short => 'l', :description => "Logging Level", :cast => :string
+  #     opt 'config.dir',:long => 'config-dir', :short => 'c', :description => "Configuration directory", :cast => :string
+  #   end
+  #
+  #   o = MyOptions.new( 'log-level' => 'debug', 'config-dir' => '/tmp/foo' )
+  #   o.properites # => Properties instance with 'log.level' => debug and 'config.dir' => '/tmp/foo'
+  #
   class Options < ::Yacl::Loader
 
-    # Internal:
+    # Internal: access to the defined list of options
+    #
+    # Returns the Array of options
     def self.opt_list
-      @opt_list||= []
+      @opt_list ||= []
     end
 
-    # Public
-    def self.opt(name, p = {} )
-      opt_list << Option.new( name, p[:long], p[:short], p[:description ], p[:cast] )
+    # Public: Declare a property and its corresponding commandline options.
+    #
+    # name   - The property name as a String
+    # params - The Hash options to accompany the property name (default: {})
+    #          :long        - The long commandline option. Without leading dashes
+    #          :short       - The short commandline option. Without leading dashes
+    #          :description - The text description of this commandline option
+    #          :cast        - What to cast the value of the commandline option
+    #                         to. This can be one of: :integer, :float, :string,
+    #                         :boolean
+    #
+    # Examples:
+    #
+    #   opt 'app.thing.one', :long => 'thing-one', :short => 't', :description => "Thing One", :cast => :string
+    #
+    # Returns nothing.
+    def self.opt(name, params = {} )
+      opt_list << Option.new( name, params[:long], params[:short], params[:description ], params[:cast] )
     end
 
-    # Public
-    def self.banner( *args )
-      @banner = args.first unless args.empty?
-      @banner ||= "Usage  :  #{File.basename($0)} [options]+\nOptions:"
-    end
-
-    # Public
+    # Public: Load the Properties from the options that were passed to the
+    # initializer.
+    #
+    # Returns a Properties instance.
     def properties
       load_properties( @options )
     end
 
-    def banner
-      self.class.banner
-    end
-
+    # Public: yield each defined Option in this class to the caller
+    #
+    # Yields the Option instance of the iteration.
+    #
+    # Returns nothing.
     def each
-      self.class.opt_list.each do |o|
+      opt_list.each do |o|
         yield o
       end
     end
 
-    private
-
-    # Given the input hash and a key prefix, load the hash into a the @map
+    # Internal: Return the list of defined options for this Class.
     #
+    # Returns an Array of Option instances
+    def opt_list
+      self.class.opt_list
+    end
+
+    # Internal: convert the given hash of long options into a Properties
+    # instance using the defined options.
+    #
+    # It is assumed that the commandline has already been parsed and that the
+    # hash passed in is the long options that were parsed out of the
+    # commandline.
+    #
+    # hash - a Hash of long options and their values.
+    #
+    # Returns a Properties instance
     def load_properties( hash )
       prop_hash = {}
       opt_list.each do |o|

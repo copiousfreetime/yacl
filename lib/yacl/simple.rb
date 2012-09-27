2f6eb524ef7c816a61707de3a1feb06240fa2d1f
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-09-27 15:26:18 -0600

    extract out defining a nested class

--- lib/yacl/simple.rb
+++ lib/yacl/simple.rb
@@ -1,20 +1,25 @@
 module Yacl
   class Simple
-
     class << self
       # Public: Create a child class of Yacl::Define::Defaults
       def defaults( &block )
-        klass = Class.new( Yacl::Define::Defaults )
-        klass.class_eval( &block )
-        return klass
+        nested_class( 'Defaults', Yacl::Define::Defaults, &block )
       end
 
       # Public: Creates a child class of Yacl::Define::Cli::Parser
       def parser( &block )
-        klass = Class.new( Yacl::Define::Cli::Parser )
-        klass.class_eval( &block )
-        return klass
+        nested_class( 'Parser', Yacl::Define::Cli::Parser, &block )
       end
+      # Internal: Define a class that is a nested class of the module
+      def nested_class( name, parent, &block )
+        unless const_defined?( name ) then
+          klass = Class.new( parent )
+          klass.class_eval( &block )
+          const_set( name , klass )
+        end
+        return const_get( name )
+      end
+
     end
   end
 end
